9a6bab6a433830828d01ca618ad2c6fbf9673913
Satish V <brahmakumarsatish@gmail.com>
2021-04-23 13:19:27 +0530

    End of section 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,26 +2,30 @@ import React, { Fragment, useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar ';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
 import { observer } from 'mobx-react-lite';
-import { useStore } from '../stores/store';
+import { Route } from 'react-router';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import { useLocation } from 'react-router-dom';
 
 function App() {
-
-  const{activityStore}=useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();    
-    }, [activityStore])
-
-  if(activityStore.loadingInitial) return <LoadingComponent content='Loading App'/>
-
+  const location = useLocation();
   return (
     <Fragment>
-      <NavBar />
-      <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard/>
-      </Container>
+      <Route exact path='/' component={HomePage} />
+      <Route path={'/(.+)'}
+        render={() => (
+          <>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+            </Container>
+          </>
+        )}
+      />
     </Fragment>
   );
 }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,12 +5,13 @@ import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
+    </BrowserRouter>
   </StoreContext.Provider>,
   document.getElementById('root')
 );
